a5d787bcddf2c640148789e17424c4ddc936c5f0
JohnDohnut <lgj1089@gmail.com>
2025-04-05 16:46:35 +0900

    pull request template added

--- src/main/java/com/dev/vault96/controller/SharedController.java
+++ src/main/java/com/dev/vault96/controller/SharedController.java
@@ -33,6 +33,7 @@ public class SharedController {
         String email = authService.extractEmailFromToken(request);
         List<SharedDocumentFolder> sharedFolders = sharedDocumentFolderService.findSharedDocumentFoldersByOwner(email);
         GetSharedDocumentFoldersResponse response = new GetSharedDocumentFoldersResponse();
+        response.setSharedDocumentFolders(sharedFolders);
         return ResponseEntity.ok(response);
     }
 
